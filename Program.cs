789ba2458295418a604b8a369e942de3d646cb67
Lucas Oliveira Souza <lucasosouza41@gmail.com>
2026-03-06 15:28:38 -0300

    Adicionando novos desafios ao projeto

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ else
 }
 */
 
-
+//Valor da compra:
 /*Console.Write("Digite o valor da sua compra: ");
 double valorCompra = double.Parse();
 
@@ -32,7 +32,8 @@ else
 */
 
 
-Console.Write("Digite o saldo na sua conta: ");
+//Verificando saldo na conta:
+/*Console.Write("Digite o saldo na sua conta: ");
 decimal saldo = decimal.Parse(Console.ReadLine());
 
 if (saldo > 0)
@@ -43,3 +44,88 @@ else
 {
     Console.WriteLine("Saldo negativo!");
 }
+*/
+
+
+//Classificação de produtos:
+/*Console.Write("Digite o código do produto (1 ou 2): ");
+int codigo = int.Parse(Console.ReadLine());
+
+if (codigo == 1)
+{
+    Console.WriteLine("O produto é peresível");
+} 
+else if (codigo == 2)
+{
+    Console.WriteLine("O produto é não peresível");
+}
+else
+{
+    Console.WriteLine($"Você digitou o código {codigo}, esse código é inválido!1");
+}
+*/
+
+
+//Classificando a nota de um aluno:
+/*Console.Write("Digite a nota final do aluno: ");
+double nota = double.Parse(Console.ReadLine());
+
+if (nota >= 9)
+{
+    Console.WriteLine("O aluno recebeu nota A");
+}
+else if (nota >= 7 && nota < 9)
+{
+    Console.WriteLine("O aluno recebeu nota B");
+}
+else if (nota >= 5 && nota < 7)
+{
+    Console.WriteLine("O aluno recebeu nota C");
+}
+else
+{
+    Console.WriteLine("O aluno recebeu nota D");
+}
+*/
+
+
+//Verificando acessoa área restrita:
+/*int senha = 1234;
+Console.Write("Digite a senha: ");
+int senhaDigitada = int.Parse(Console.ReadLine());
+
+Console.Write("Digite o nível de acesso: ");
+int nivelAcesso = int.Parse(Console.ReadLine());
+
+if (senha == senhaDigitada && nivelAcesso >= 5)
+{
+    Console.WriteLine("Acesso liberado");
+}
+else
+{
+    Console.WriteLine("Acesso negado");
+}
+*/
+
+
+//Classificando a faixa etária
+/*Console.Write("Digite a sua idade: ");
+int idade = int.Parse(Console.ReadLine());
+
+if (idade <= 12)
+{
+    Console.WriteLine("Classificação: Infatil");
+}
+else if (idade >= 13 && idade <= 17)
+{
+    Console.WriteLine("Classificação: Adolescente");
+}
+else if (idade >= 18 && idade <= 59)
+{
+    Console.WriteLine("Classificação: Adulto");
+}
+else
+{
+    Console.WriteLine("Classificação: Idoso");
+}
+*/
